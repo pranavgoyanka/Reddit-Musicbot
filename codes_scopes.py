4995d7d981be073378fc01eed3c4fa3242883e72
Pranav Goyanka <pgoyanka@gmail.com>
2019-04-12 02:15:47 +0530

    Basic reddit side authorisation working

---
+++ codes_scopes.py
@@ -0,0 +1,1 @@
+read = 'your_code_here'
---
+++ main.py
@@ -0,0 +1,48 @@
+import praw
+import prawcreds as pc
+import codes_scopes as scopes
+
+client_id = pc.client_id
+client_secret = pc.client_secret
+password = pc.password
+user_agent = pc.user_agent
+username = pc.username
+redirect_uri = pc.redirect_uri
+
+reddit = praw.Reddit(client_id = client_id,
+					 client_secret = client_secret,
+					 password = password,
+					 user_agent = user_agent,
+					 username = username, 
+					 redirect_uri = redirect_uri)
+
+# important scopes
+# read - read content from posts
+# submit - submit content, basically write posts and comments
+# edit - edit posts and content
+# all oauth scopes link - https://www.reddit.com/r/GoldTesting/comments/3chbrm/all_oauth_scopes/
+
+scope = 'read'
+
+subreddit = reddit.subreddit('testingground4bots')
+
+# obtain a permanent token
+
+url = reddit.auth.url([scope], '...', 'permanent')
+print(url)
+
+# assign code the rerturn url from the auth url
+# code = '4eJnpglTjtOAX6Wgia5ciE8LGgo' 
+
+# using already obtained code
+code = scopes.read
+print(code)
+print('\n')
+
+# Getting authorisation
+reddit.auth.authorize(code)
+
+for submissions in reddit.subreddit('all').hot(limit=25):
+	print(submissions.title)
+
+
